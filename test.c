48478e82c9189a8123b1b3ec0cb3a5ab6320bf26
Danilo Horta <danilo.horta@gmail.com>
2019-06-24 14:32:09 +0100

    avx2

--- test.c
+++ test.c
@@ -2,11 +2,6 @@
 #include <stdio.h>
 #include <stdint.h>
 int main(void) {
-  __m256i v1 = _mm256_set1_epi32(42);
-  __m256i v2 = _mm256_set1_epi32(214);
-  union { __m256i v; int32_t x[8]; } v3;
-  v3.v = _mm256_add_epi32(v1, v2);
-  printf("%d\n", v3.x[2]);
   union { __m256i v; uint8_t x[32]; } u;
   __m256i a = u.v;
   a = _mm256_max_epu8(a, _mm256_permute2x128_si256(a, a, 0x01));
