370d1ef2fa74ea69de6157831954414ebff7ffbe
Danilo Horta <danilo.horta@gmail.com>
2019-06-24 16:08:36 +0100

    test.c

--- test.c
+++ test.c
@@ -1,12 +1,10 @@
 #include <x86intrin.h>
-#include <stdio.h>
 #include <stdint.h>
 int main(void) {
   __m256i v1 = _mm256_set1_epi32(42);
   __m256i v2 = _mm256_set1_epi32(214);
   union { __m256i v; int32_t x[8]; } v3;
   v3.v = _mm256_add_epi32(v1, v2);
-  printf("%d\n", v3.x[2]);
-  return 0;
+  int status = ((int) v3.x[2]) == 256;
+  return !status;
 }
-
